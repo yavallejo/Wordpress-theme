a9f64213587fff52a195791e971e5d6481d9be59
aprendiendoandoo@gmail.com <aprendiendoandoo@gmail.com>
2015-10-25 12:11:04 -0500

    Clase 11 - Adicionamos el Metaslider

--- header.php
+++ header.php
@@ -42,10 +42,9 @@
 
                         <!-- Banner -->
                             <section id="banner">
-                                <header>
-                                    <h2>Howdy. This is Dopetrope.</h2>
-                                    <p>A responsive template by HTML5 UP</p>
-                                </header>
+                                <?php
+                                    echo do_shortcode("[metaslider id=63]");
+                                ?>
                             </section>
 
                         <!-- Intro -->
